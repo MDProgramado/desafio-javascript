c6de13ed5955d14d2a26dcf3695d0632fa6d082e
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-21 16:51:15 -0300

    adionando os elementos no javaSCript

--- js/carousel.js
+++ js/carousel.js
@@ -22,7 +22,7 @@ class Carousel {
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },3000);
+                Carousel._interval = setInterval(function(){ Carousel.Next(); },2000);
             }
             
         } else {
@@ -30,32 +30,42 @@ class Carousel {
         }
     }
     
-    function  playBotoes(idElementButton) {
-        document.querySelector(idElementButton).onclick()
-    }
-    const ListaBotoesControle = document.querySelectorAll('.botoes-carousel');
-    
-    for(const i = 0; i < botoesControle.length; i++) {
-        const botao = ListaBotoesControle[i];
-        const direcao = tecla.classList[1];
-        const idBotao = `#botao-${direcao}`;
-
-        tecla.onclick = function() {
-            playBotoes(idBotao);
-        }
-    }
- 
-
-
     static Next(carouselArr){ 
         const carouselDiv = document.getElement('carousel');
         const carouselTitleDiv = document.getElementById('carousel-title');
 
         carouselArr.push(new Carousel("imagem_1.jpg","Esta é a nova Ranger Ford 2022. Verifique novidades.","lancamento.html"));
-        carouselArr.push(new Carousel("imagem_2.jpg","Ford a nossa história","#"));
-        carouselArr.push(new Carousel("imagem_3.jpg","Nova Ford Bronco Sport 2022","lancamento.html"));
-        Carousel.Start(carouselArr);
-        
-        
+            carouselArr.push(new Carousel("imagem_2.jpg","Ford a nossa história","#"));
+            carouselArr.push(new Carousel("imagem_3.jpg","Nova Ford Bronco Sport 2022","lancamento.html"));]
+
+           
+            for(let i = 0; i < carouselArr.length; i++) {
+                let img = document.createElement("img")
+                img.setAttribute("src", carouselArr[i].img);
+                document.getElementById("carousel").insertAdjacentElement("beforeend", img)
+
+                let texto = document.createElement("a")
+                texto.innerHTML = carouselArr[i].titulo
+                document.getElementById("carousel-title").insertAdjacentElement("beforeend", texto)            
+            }
+
     }
 };
+
+Carousel.Start(carouselArr);
+        
+
+ // function  playBotoes(idElementButton) {
+    //     document.querySelector(idElementButton).onclick()
+    // }
+    // const ListaBotoesControle = document.querySelectorAll('.botoes-carousel');
+    
+    // for(const i = 0; i < botoesControle.length; i++) {
+    //     const botao = ListaBotoesControle[i];
+    //     const direcao = tecla.classList[1];
+    //     const idBotao = `#botao-${direcao}`;
+
+    //     tecla.onclick = function() {
+    //         playBotoes(idBotao);
+    //     }
+    // }
