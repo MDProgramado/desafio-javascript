c2986f96d72522d42e025e11c87e98bb5c3e9d58
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-25 09:36:43 -0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -4,78 +4,52 @@
 
 //Array storage class
 
-const carouselArr = [];
+let carouselArr = [];
 class Carousel {
     constructor(image, title, url) {
-        this.image = image;
-        this.title = title;
-        this.url = url;
+        this.Image = image;
+        this.Title = title;
+        this.Url = url;
     }
-      
-    static Start(arr){
-        if(arr){
-            if(arr.length > 0){
+
+    static Start(arr) {
+        if (arr) {
+            if (arr.length > 0) {
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
-                Carousel.Next(arr); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(arr); }, 2000);
+                Carousel.Next(arr);
+                Carousel._interval = setInterval(function () { Carousel.Next(arr); }, 2000);
             }
         } else {
-            throw "Method Start needs an Array Variable.";
+            throw "Method Start need a Array Variable.";
         }
     }
-    
+
     static Next(carouselArr) {
-        const carouselDiv = document.getElementById('carousel');
-        const carouselTitleDiv = document.getElementById('carousel-title');
+        const carouselSlide = document.getElementById('carousel');
+        const carouselTitle = document.getElementById('carousel-title');
 
-        if(Carousel._sequence === 0) {
-            
-            const newItems = [
-                new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"),
-                new Carousel("imagem_2.jpg", "Ford a nossa história", "#"),
-                new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html")
-            ];
-    
-            carouselArr.push(...newItems);
-    
-            carouselArr.forEach(item => {
-                Carousel.appendImage(carouselDiv, item.image);
-                Carousel.appendTitle(carouselTitleDiv, item.title, item.url);
-            });
+        const newItems[
+            carouselArr.push(new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"));
+            carouselArr.push(new Carousel("imagem_2.jpg", "Ford a nossa história", "index.html"));
+            carouselArr.push(new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html"));
+        ]
+
+        if (carouselSlide && carouselTitle) {
+            const slide = carouselArr[Carousel._sequence];
+
+            carouselSlide.style.backgroundImage = `url('img/${slide.Image}')`;
+            carouselSlide.style.backgroundSize = 'cover';
+            carouselSlide.innerHTML = '';
+
+            carouselTitle.innerHTML = `<a href="${slide.Url}">${slide.Title}</a>`;
+
+            Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
         }
-
-        // Atualiza a imagem ativa no carrossel
-        const images = carouselDiv.getElementsByTagName("img");
-        const titles = carouselTitleDiv.getElementsByTagName("a");
-
-        Array.from(images).forEach(img => img.classList.remove("active"));
-        Array.from(titles).forEach(title => title.classList.remove("active"));
-
-        images[Carousel._sequence].classList.add("active");
-        titles[Carousel._sequence].classList.add("active");
-
-        Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
     }
-
-    static appendImage(parent, src) {
-        const img = document.createElement("img");
-        img.setAttribute("src", src);
-        parent.appendChild(img);
-    }
-
-    static appendTitle(parent, title, link) {
-        const anchor = document.createElement("a");
-        anchor.innerHTML = title;
-        anchor.setAttribute("href", link);
-        parent.appendChild(anchor);
-    }
-}
-
-// Exemplo de uso:
+};
 
 Carousel.Start(carouselArr);
-        
 
  // function  playBotoes(idElementButton) {
     //     document.querySelector(idElementButton).onclick()
