6f9224618c3fa7b4e707729030c43b6ab2fcda14
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-25 10:01:32 -0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,13 +1,6 @@
 
 
 
-let carouselArr = [
-    new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"),
-    new Carousel("imagem_2.jpg", "Ford a nossa história", "index.html"),
-    new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html")
-];
-
-//class Carousel
 class Carousel {
     constructor(image, title, url) {
         this.Image = image;
@@ -16,40 +9,36 @@ class Carousel {
     }
 
     static Start(arr) {
-        if (arr) {
-            if (arr.length > 0) {
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(arr);
-                Carousel._interval = setInterval(function () { Carousel.Next(arr); }, 2000);
-            }
+        if (arr && arr.length > 0) {
+            Carousel._sequence = 0;
+            Carousel._size = arr.length;
+            Carousel.Next(arr); //Start
+            Carousel._interval = setInterval(() => Carousel.Next(arr), 2000);
         } else {
-            throw "Method Start need a Array Variable.";
+            console.error("O carrossel está vazio ou não foi fornecido um array.");
         }
     }
 
-    static Next(carouselArr) {
+    static Next(arr) {
         const carouselSlide = document.getElementById('carousel');
         const carouselTitle = document.getElementById('carousel-title');
 
-       
-        
         if (carouselSlide && carouselTitle) {
-            const slide = carouselArr[Carousel._sequence];
-
+            const slide = arr[Carousel._sequence];
             carouselSlide.style.backgroundImage = `url('img/${slide.Image}')`;
             carouselSlide.style.backgroundSize = 'cover';
-            carouselSlide.innerHTML = '';
-
             carouselTitle.innerHTML = `<a href="${slide.Url}">${slide.Title}</a>`;
-
             Carousel._sequence = (Carousel._sequence + 1) % Carousel._size;
         }
     }
-};
+}
 
 
+const carouselArr = [
+    new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"),
+    new Carousel("imagem_2.jpg", "Ford a nossa história", "index.html"),
+    new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html")
+];
 
 
-// Inicia o carrossel
 Carousel.Start(carouselArr);
