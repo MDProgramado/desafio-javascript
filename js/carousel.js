8110aa2df3443a1bda34cd85b3b6d20843843177
Maicon Douglas <mdprogamacao@outlook.com>
2025-03-25 09:50:20 -0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -28,11 +28,7 @@ class Carousel {
         const carouselSlide = document.getElementById('carousel');
         const carouselTitle = document.getElementById('carousel-title');
 
-        const newItems = [
-            carouselArr.push(new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"));
-            carouselArr.push(new Carousel("imagem_2.jpg", "Ford a nossa história", "index.html"));
-            carouselArr.push(new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html"));
-        ]
+       
         
         if (carouselSlide && carouselTitle) {
             const slide = carouselArr[Carousel._sequence];
@@ -47,5 +43,11 @@ class Carousel {
         }
     }
 };
+
+ const newItems = [
+            carouselArr.push(new Carousel("imagem_1.jpg", "Esta é a nova Ranger Ford 2022. Verifique novidades.", "lancamento.html"));
+            carouselArr.push(new Carousel("imagem_2.jpg", "Ford a nossa história", "index.html"));
+            carouselArr.push(new Carousel("imagem_3.jpg", "Nova Ford Bronco Sport 2022", "lancamento.html"));
+        ]
 
 Carousel.Start(carouselArr);
